4a6b8bcbfe561a79e3b7e30b579acdc8be85895b
Yash <yash592@gmail.com>
2017-10-28 18:58:55 -0700

    Logic done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
   //your code here
 
 
-
+counter = 0;
+wins=0;
+losses=0;
 
 var NumberToGuess = Math.floor((Math.random() * 100) + 1);
 
@@ -26,6 +28,35 @@ mineral.attr("mineral-points", imagepoints[i]);
 $("#minerals").append(mineral)
 
 }
+
+$(".mineral-image").on("click", function(){
+
+
+
+var MineralPoints = ($(this).attr("mineral-points"));
+mineralvalue = parseInt(MineralPoints);
+
+counter += mineralvalue;
+
+console.log(counter);
+
+if (counter == NumberToGuess) {
+	alert("You won!");
+	wins++;
+	$("#win").text("Wins: " + wins)
+}
+
+else if (counter > NumberToGuess) {
+	alert("You lose!");
+	losses++;
+	$("#loss").text("Losses: " + losses)
+}
+
+
+
+
+
+})
 
 
 });
